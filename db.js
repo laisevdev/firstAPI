32f86aec2797083eae62069d62f44c829d202e77
Laíse <laisevdev@gmail.com>
2023-09-25 20:36:20 -0400

    create db in the memory and functions selectCustomers

--- db.js
+++ db.js
@@ -1,1 +1,31 @@
-const clientes = [];
+const customers = [{
+    id: 1,
+    nome: "Suzana",
+    idade: 24,
+    uf: "MT"
+},
+{
+    id: 2,
+    nome:"Giovana",
+    idade:18,
+    uf: "MT"
+},
+{
+    id: 3,
+    nome:"Julia",
+    idade:38,
+    uf: "MT"
+}];
+
+function selectCustomers(){
+    return customers;
+}
+
+function selectCustomer(id){
+    return customers.find(c => c.id === id);
+}
+
+module.exports = {
+    selectCustomers,
+    selectCustomer
+}
